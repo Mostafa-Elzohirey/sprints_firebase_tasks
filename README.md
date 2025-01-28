fbb41a96a3b56d2b25013c15e165620ee28baa8e
MostafaElzohirey <132284280+Mostafa-Elzohirey@users.noreply.github.com>
2025-01-28 17:04:38 +0200

    task solution (uploaded the full project to make testing it with the current  project easier)

---
+++ README.md
@@ -0,0 +1,7 @@
+# sprints_firebase_tasks
+Added firebase authentication to the shopping app
+this ui was the [Shopping app](https://github.com/Mostafa-Elzohirey/sprints_flutter_task_5/blob/master/README.md) ui with all of its features (animations and localization)
+
+## Added Features:
+- Added firebase creating a user when signing up
+- Added firebase validation using the signInWithEmailAndPassword function when logging in           
---
+++ lib/main.dart
@@ -0,0 +1,54 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_localization/flutter_localization.dart';
+import 'package:sprints_firebase_tasks/login_page.dart';
+
+import 'firebase_options.dart';
+import 'locales.dart';
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await FlutterLocalization.instance.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  runApp(const MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  void _onTranslatedLanguage(Locale? locale) {
+    setState(() {});
+  }
+
+  final FlutterLocalization localization = FlutterLocalization.instance;
+
+  @override
+  void initState() {
+    localization.init(
+      mapLocales: [
+        const MapLocale('en', AppLocale.en),
+        const MapLocale('ar', AppLocale.ar),
+      ],
+      initLanguageCode: 'en',
+    );
+    localization.onTranslatedLanguage = _onTranslatedLanguage;
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Firebase App',
+      supportedLocales: localization.supportedLocales,
+      localizationsDelegates: localization.localizationsDelegates,
+      home: LoginPage(localization: localization),
+    );
+  }
+}
