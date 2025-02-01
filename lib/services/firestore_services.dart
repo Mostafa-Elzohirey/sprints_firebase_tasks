37e5a020ab41c6284dab524e871daa8da0133d79
Mostafa-Elzohirey <132284280+Mostafa-Elzohirey@users.noreply.github.com>
2025-02-01 23:12:59 +0200

    Update firestore_services.dart

--- lib/services/firestore_services.dart
+++ lib/services/firestore_services.dart
@@ -7,28 +7,6 @@ class FirestoreServices {
   FirestoreServices._privateConstructor();
   static final _fireStore = FirebaseFirestore.instance;
 
-  //create
-  static Future<UserModel> addUser(UserModel user) async {
-    await _fireStore.collection('users').doc(user.id).set(user.toFirestore());
-    return user;
-  }
-
-  //fetch
-  static Future<UserModel> getUser(UserModel userModel) async {
-    final retrievedUser =
-        await _fireStore.collection('users').doc(userModel.id).get();
-    final user = UserModel.fromFirestore(retrievedUser, null);
-    return user;
-  }
-
-  //update
-  static Future<void> updateUser(UserModel user) async {
-    await _fireStore
-        .collection('users')
-        .doc(user.id)
-        .update(user.toFirestore());
-  }
-
   static Future<void> addData(Map<String, String> data) async {
     final id = DateTime.now().millisecondsSinceEpoch.toString();
     await _fireStore.collection('data').doc(id).set(data);
